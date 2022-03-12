252dda4818c5306b9d28cd3d3603342e1e3a3c85
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-12 14:30:05 -0400

    Initial interface for Perceptron

---
+++ Perceptron.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <variant>
+#include <vector>
+#include "error.hpp"
+
+#define multitype std::variant<double, int, long long, std::string, char, bool>
+
+class Perceptron{
+    private:
+        std::vector<double> w;
+        double tol;
+        int max_iter, n_iter, random_state;
+        std::string error;
+        void initialize_weights(int n);
+        int step_function(double y_);
+
+    public:
+        Perceptron(int max_iter=10, int n_iter=5, std::string error="mae", double tol=1e-3, double lr=0.1, int random_state=0);
+        void fit(std::vector<std::vector<multitype>>X, std::vector<multitype>y);
+        int predict();
+};
