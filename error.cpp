78585d811f3dd693602d48395e0b7a961bbd0ac8
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-12 13:07:06 -0400

    Finished MeanAbsoluteError

--- error.cpp
+++ error.cpp
@@ -1,5 +1,20 @@
 #include "error.hpp"
 
-double rmse();
-double mse();
-double mae();
+double rmse(std::vector<double> y_, std::vector<double> y){
+
+}
+
+double mse(std::vector<double> y_, std::vector<double> y){
+
+}
+
+double mae(std::vector<double> y_, std::vector<double> y){
+    double error=0;
+
+    for (int i=0; i<y_.size(); i++){
+        error += abs(y_[i]-y[i]);
+    }
+    error /= y.size();
+
+    return error;
+}
