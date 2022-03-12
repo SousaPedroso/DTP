599a6573d23a83283cdda572d392befd1da71695
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-12 13:29:44 -0400

    Finished RootMeanSquareError

--- error.cpp
+++ error.cpp
@@ -1,7 +1,15 @@
 #include "error.hpp"
 
 double rmse(std::vector<double> y_, std::vector<double> y){
+    double error=0;
 
+    for (int i=0; i<y_.size(); i++){
+        error += pow(y_[i]-y[i], 2);
+    }
+    error /= y.size();
+    error = sqrt(error);
+
+    return error;
 }
 
 double mse(std::vector<double> y_, std::vector<double> y){
