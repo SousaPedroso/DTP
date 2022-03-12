bcb1ce2ad7277baef922d75faa7b3cd7cca9a504
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-12 12:55:09 -0400

    Update: Interface for errors

--- error.hpp
+++ error.hpp
@@ -1,5 +1,7 @@
 #include <string>
+#include <vector>
+#include <cmath>
 
-double rmse();
-double mse();
-double mae();
+double rmse(std::vector<double> y_, std::vector<double> y);
+double mse(std::vector<double> y_, std::vector<double> y);
+double mae(std::vector<double> y_, std::vector<double> y);
