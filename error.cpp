d90975a12e48a7bcd72e4eb2ea506ef56bdb0c8c
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-12 13:26:30 -0400

    Finished MeanSquaredError

--- error.cpp
+++ error.cpp
@@ -5,7 +5,14 @@ double rmse(std::vector<double> y_, std::vector<double> y){
 }
 
 double mse(std::vector<double> y_, std::vector<double> y){
+    double error=0;
 
+    for (int i=0; i<y_.size(); i++){
+        error += pow(y_[i]-y[i], 2);
+    }
+    error /= y.size();
+
+    return error;
 }
 
 double mae(std::vector<double> y_, std::vector<double> y){
