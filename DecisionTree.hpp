90519c929f2c672dd11e2127ea9a954cb93a2774
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-13 17:47:57 -0400

    Update: Set name for the features

--- DecisionTree.hpp
+++ DecisionTree.hpp
@@ -8,8 +8,9 @@
 class DecisionTree{
     private:
         int n_features, n_classes;
-        std::string criterion;
+        std::string criterion, target_name;
         std::vector<Node> tree;
+        std::vector<std::string> feature_names;
 
         std::map<multitype, std::map<multitype, int>> get_values(std::vector<multitype> sample, std::vector<multitype> classes);
         // Get the classes for each attribute
@@ -18,6 +19,7 @@ class DecisionTree{
         double gain(double entropy1, double entropy2);
     public:
         DecisionTree(std::string criterion);
+        void names(std::vector<std::string>feature_names, std::string target_name);
         void fit(std::vector<std::vector<multitype>>X, std::vector<multitype>y);
         multitype predict(std::vector<multitype>X);
         std::vector<multitype> predict(std::vector<std::vector<multitype>>X);
