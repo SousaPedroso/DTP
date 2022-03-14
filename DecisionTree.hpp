5be3463f28a57228326d390e95009b92f08f08d9
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-14 14:00:39 -0400

    Update: Pointer to root and storing of features

--- DecisionTree.hpp
+++ DecisionTree.hpp
@@ -3,14 +3,17 @@
 #include <iostream>
 #include <cmath>
 #include <map>
+#include <stack>
 #include <utility>
 
 class DecisionTree{
     private:
         int n_features, n_classes;
         std::string criterion, target_name;
-        std::vector<Node> tree;
+        Node* tree;
         std::vector<std::string> feature_names;
+        // Store the feature nodes to merge the edge and a feature node
+        std::stack<Node> features;
 
         std::map<multitype, std::map<multitype, int>> get_values(std::vector<multitype> sample, std::vector<multitype> classes);
         // Get the classes for each attribute
@@ -23,5 +26,5 @@ class DecisionTree{
         void fit(std::vector<std::vector<multitype>>X, std::vector<multitype>y);
         multitype predict(std::vector<multitype>X);
         std::vector<multitype> predict(std::vector<std::vector<multitype>>X);
-        std::vector<Node> get_tree();
+        Node* get_tree();
 };
