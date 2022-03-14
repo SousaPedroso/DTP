76ecfea8492998b776c7de54d087efb2c5f92ccc
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-14 09:30:08 -0400

    Update: Get index of a node and his adjacents nodes

--- Node.cpp
+++ Node.cpp
@@ -1,7 +1,8 @@
 #include "Node.hpp"
 
-Node::Node(std::string label, multitype value){
+Node::Node(std::string label, int index, multitype value){
     this->label = label;
+    this->index = index; // Stores the index, considering the data containing it
     this->value = value;
 }
 
@@ -12,6 +13,14 @@ std::string Node::get_label(){
     return this->label;
 }
 
+int Node::get_index(){
+    return this->index;
+}
+
 void Node::set_adjacent(Node no){
     this->adjacents.push_back(no);
+}
+
+std::vector<Node> Node::get_adjacents(){
+    return this->adjacents;
 }
--- Node.hpp
+++ Node.hpp
@@ -7,6 +7,7 @@
 
 class Node{
     private:
+        int index;
         std::string label;
         // Acceptable types for value
         multitype value;
@@ -14,8 +15,10 @@ class Node{
         std::vector<Node> adjacents;
         // Index to track the values coming to decision tree and goes through
     public:
-        Node(std::string label, multitype value);
+        Node(std::string label, int index, multitype value);
         ~Node();
         std::string get_label();
+        int get_index();
         void set_adjacent(Node no);
+        std::vector<Node> get_adjacents();
 };
