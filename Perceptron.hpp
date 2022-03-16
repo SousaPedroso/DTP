a33854e1ae8651651d09a8c709b5bbaa591c16da
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-16 15:04:09 -0400

    Update: verbose must be passed as an argument

--- Perceptron.hpp
+++ Perceptron.hpp
@@ -18,8 +18,8 @@ class Perceptron{
         int step_function(double y_);
 
     public:
-        Perceptron(int max_iter=10, int n_iter=5, std::string error="mae", double tol=1e-3,
-                    double lr=0.1, int random_state=0, bool verbose=false);
+        Perceptron(bool verbose, int max_iter=10, int n_iter=5, std::string error="mae", double tol=1e-3,
+                    double lr=0.1, int random_state=0);
         void fit(std::vector<std::vector<double>>X, std::vector<double>y);
         int predict(std::vector<double>X);
         std::vector<int> predict(std::vector<std::vector<double>>X);
