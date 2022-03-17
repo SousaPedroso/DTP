dab278baae068256edc985b0a00550862dd06ffd
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-17 13:06:47 -0400

    Update: Method to transpose a matrix

--- utils.hpp
+++ utils.hpp
@@ -5,3 +5,4 @@
 
 std::vector<std::string> split_string(std::string line, const char delimiter= ' ');
 std::vector<std::string> split_string(std::string line, std::string delimiter);
+std::vector<std::vector<std::string>> transpose(std::vector<std::vector<std::string>>m);
