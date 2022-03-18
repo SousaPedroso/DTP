df2078679e29de85b43a3da0c6b952ac145deae9
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2022-03-17 21:39:26 -0400

    Fixed problems to compute entropy and predict data
    Updated entropy for an attribute or a set

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -5,10 +5,13 @@ DecisionTree::DecisionTree(std::string criterion){
     this->n_classes = 0;
 }
 
-std::map<multitype, std::map<multitype, int>> DecisionTree::get_values(std::vector<multitype> sample, std::vector<multitype> classes){
+// Destroys the tree
+DecisionTree::~DecisionTree() = default;
+
+std::map<std::string, std::map<std::string, int>> DecisionTree::class_association(std::vector<std::string> sample, std::vector<std::string> classes){
     // Stores the number of diferent classes, the first map is for the feature and second one to the class
-    std::map<multitype, std::map<multitype, int>> differences;
-    std::map<multitype, std::map<multitype, int>>::iterator d_iterator;
+    std::map<std::string, std::map<std::string, int>> differences;
+    std::map<std::string, std::map<std::string, int>>::iterator d_iterator;
 
     // TO-DO: Stores the index in the dataset to fast check to compute entropy
 
@@ -22,59 +25,94 @@ std::map<multitype, std::map<multitype, int>> DecisionTree::get_values(std::vect
                 differences[sample[i]][classes[i]]++;
             }
             else{
-                differences[sample[i]][classes[i]] = 0;
+                differences[sample[i]][classes[i]] = 1;
             }
         }
         // Attribute First time seen
         else{
-            differences[sample[i]][classes[i]] = 0;
+            differences[sample[i]][classes[i]] = 1;
         }
     }
     return differences;
 }
 
-double DecisionTree::entropy(std::map<multitype, std::map<multitype, int>>values, int samples){
+std::map<std::string, int> DecisionTree::c_distribution(std::vector<std::string> classes){
+    std::map<std::string, int> distribution;
+
+    for (auto class_value: classes){
+        if (distribution.find(class_value) != distribution.end()){
+            distribution[class_value] ++;
+        }
+        else {
+            distribution[class_value] = 1;
+        }
+    }
+
+    return distribution;
+}
+
+double DecisionTree::entropy_S(std::map<std::string, int>values, int samples){
     double e=0;
+
+    for (auto target: values){
+        e += -1*((double)target.second/(double)samples)*log2((double)target.second/(double)samples);
+    }
+    return e;
+}
+
+double DecisionTree::entropy_a(std::map<std::string, std::map<std::string, int>>values, int samples){
+    double e=0, e_i;
     // Checks the counting of samples for a feature
     int attribute_count;
 
     for (auto attribute_value: values){
         attribute_count = 0;
+        e_i=0;
         // Update the counting of samples for a feature
         for (auto class_value: attribute_value.second){
             attribute_count += class_value.second;
         }
         // Consider each class associated with an attribute value
         for (auto class_value: attribute_value.second){
-            e += (class_value.second/attribute_count)* log2(class_value.second/attribute_count);
+            e_i += -1*((double)class_value.second/(double)attribute_count)* log2((double)class_value.second/(double)attribute_count);
         }
-        e += (attribute_count/samples)*(-e);
+        e_i *= ((double)attribute_count/(double)samples);
+        e += e_i;
     }
 
     return e;
 }
 
 // Gini impurity
-double DecisionTree::gini(std::map<multitype, std::map<multitype, int>>values, int samples){
+double DecisionTree::gini_S(std::map<std::string, int>values, int samples){
     double gi=1;
+    double p_i=0;
+
+    for (auto target: values){
+        p_i += ((double)target.second/(double)samples)*((double)target.second/(double)samples);
+    }
+
+    return gi-p_i;
+}
+
+double DecisionTree::gini_a(std::map<std::string, std::map<std::string, int>>values, int samples){
+    double gi=0;
+    double p_i;
     int attribute_count;
 
-    for (auto attribute_value: values){
-        attribute_count = 0;
-        // Update the counting of samples for a feature
-        for (auto class_value: attribute_value.second){
-            attribute_count += class_value.second;
+    for (auto feat_value: values){
+        p_i=0;
+        attribute_count=0;
+        for (auto s_value: feat_value.second){
+            attribute_count += s_value.second;
         }
-        // For each possibility of class, update gini impurity [(p_i)^2]
-        for (auto class_value: attribute_value.second){
-            gi -= pow(class_value.second/attribute_count, 2);
-        }
+        gi += ((double)attribute_count/(double)samples) * this->gini_S(feat_value.second, attribute_count);
     }
     return gi;
 }
 
-double DecisionTree::gain(double entropy1, double entropy2){
-    return entropy2 - entropy1;
+double DecisionTree::gain(double set, double attribute){
+    return set - attribute;
 }
 
 void DecisionTree::names(std::vector<std::string>feature_names, std::string target_name){
@@ -87,7 +125,7 @@ std::vector<std::string> DecisionTree::get_features(){
     return this->feature_names;
 }
 
-void DecisionTree::fit(std::vector<std::vector<multitype>>X, std::vector<multitype>y){
+void DecisionTree::fit(std::vector<std::vector<std::string>>X, std::vector<std::string>y){
 
     // Entropy/gini for the output classes
     double y_dispersion;
@@ -95,38 +133,40 @@ void DecisionTree::fit(std::vector<std::vector<multitype>>X, std::vector<multity
     double feature_gain;
     // Computes the gain in execution time for the root
     double max_gain=0;
-    int index_max_gain;
+    int index_max_gain=0;
 
-    std::map<multitype, std::map<multitype, int>> y_classes = this->get_values(y, y);
-
+    std::map<std::string, int> y_distribution = this->c_distribution(y);
     // Data of one class means entropy equals 0, no dispersion (leaf)
-    if (y_classes.size() == 1){
+    if (y_distribution.size() == 1){
         // All values are equal, get the first one
-        Node leave = Node(target_name, this->n_features, y[0]);
+        std::cout<< "Leaf for "<< y[0]<< endLine;
+        Node* leave = new Node(target_name, this->n_features, y[0]);
         this->features.push(leave);
         return ;
     }
 
     if (this->criterion == "entropy"){
-        y_dispersion = this->entropy(y_classes, y.size());
+        y_dispersion = this->entropy_S(y_distribution, y.size());
     }
     else{
-        y_dispersion = this->gini(y_classes, y.size());
+        y_dispersion = this->gini_S(y_distribution, y.size());
     }
     
     // Index for each feature
     int index=0;
     // Entropy and gain for each attribute
-    for (auto feature: X){
-        std::map<multitype, std::map<multitype, int>> feature_classes = this->get_values(feature, y);
+    std::vector<std::vector<std::string>> transposed_X = transpose(X);
+
+    for (auto feature: transposed_X){
+        std::map<std::string, std::map<std::string, int>> feature_classes = this->class_association(feature, y);
         if (this->criterion == "entropy"){
-            feature_dispersion = this->entropy(y_classes, y.size());
+            feature_dispersion = this->entropy_a(feature_classes, y.size());
         }
         else{
-            feature_dispersion = this->gini(y_classes, y.size());
+            feature_dispersion = this->gini_a(feature_classes, y.size());
         }
         feature_gain = this->gain(y_dispersion, feature_dispersion);
-        if (feature_gain > max_gain){
+        if (feature_gain >= max_gain){
             max_gain = feature_gain;
             index_max_gain = index;
         }
@@ -134,103 +174,99 @@ void DecisionTree::fit(std::vector<std::vector<multitype>>X, std::vector<multity
     }
     // Add a node for a feature
     std::string label = index_max_gain +"";
-    Node feature_gain_node = Node(this->feature_names[index_max_gain], index_max_gain, -1);
+    std::cout<< "Max gain: "<<max_gain<< endLine;
+    std::cout<< "Feature of max gain: "<< this->feature_names[index_max_gain]<< endLine;
+    Node* feature_gain_node = new Node(this->feature_names[index_max_gain], index_max_gain, "-1");
 
     if (this->tree == NULL){
-        this->tree = &feature_gain_node;
+        this->tree = feature_gain_node;
     }
-    else {
-        features.push(feature_gain_node);
-    }
+    this->features.push(feature_gain_node);
 
     // Stores the indices for each different value
     // To proceed in DT calculating gain and the leaves
-    std::map<multitype, std::vector<int>> features_indices;
+    std::map<std::string, std::vector<int>> features_indices;
 
     // Add a node for each value in this feature
     index = 0;
-    for (auto feature_value: X[index_max_gain]){
+    for (auto feature_value: transposed_X[index_max_gain]){
         // Check if this value was found for iterate through the tree
         if (features_indices.find(feature_value) != features_indices.end()){
             features_indices[feature_value].push_back(index);
         }
         else{
             // Insert the first occurence of this value
-            features_indices.insert(std::pair<multitype, std::vector<int>>(feature_value, {index}));
+            features_indices.insert(std::pair<std::string, std::vector<int>>(feature_value, {index}));
         }
         index ++;
     }
-    // Get the targets associated to next fit
-    std::map<multitype, std::map<multitype, int>> feature_classes = this->get_values(X[index_max_gain], y);
 
     // Split the data according to the indices for solve the fit
     // for expand the tree for each type of attribute
     for (auto feature_value: features_indices){
         // Get the data associated with the feature
-        std::vector<std::vector<multitype>>splitted_X = {{}};
-        std::vector<multitype>splitted_y = {};
+        std::vector<std::vector<std::string>>splitted_X;
+        std::vector<std::string>splitted_y = {};
         // index of current data
         index = 0;
         // Get the indices containing the features, group the data and continues splitting
-        for (auto data: X){
-            // Get the values for each data at a certain index
-            for (auto feature_index: feature_value.second){
-                splitted_X[splitted_X.size()-1].push_back(data[feature_index]);
-            }
-            // Check Y associated with the feature
-            if (feature_classes[feature_value.first].find(y[index]) != feature_classes[feature_value.first].end()){
-                splitted_y.push_back(y[index]);
-            }
-            index ++;
+        for (auto feat_index: feature_value.second){
+            splitted_X.push_back(X[feat_index]);
+            splitted_y.push_back(y[feat_index]);
+            // Remove the data for the index of the feature
+            // splitted_X[splitted_X.size()-1].erase(splitted_X[splitted_X.size()-1].begin()+index_max_gain, splitted_X[splitted_X.size()-1].begin()+index_max_gain+1);
         }
         // Solve the problem for the next attributes and values
         // It is considered as a node, but it is the edge between two different attributes
-        Node edge = Node(feature_gain_node.get_label(),
-                feature_gain_node.get_index(), feature_value.first);
+        Node* edge = new Node((*feature_gain_node).get_label(),
+                (*feature_gain_node).get_index(), feature_value.first);
 
         // Edge for a feature
-        feature_gain_node.set_adjacent(edge);
+        feature_gain_node->set_adjacent(edge);
         this->fit(splitted_X, splitted_y);
         // Update with the results for each depth
-        edge.set_adjacent(this->features.top());
+        edge->set_adjacent(this->features.top());
         // Remove the element
         this->features.pop();
     }
 }
 
-multitype DecisionTree::predict(std::vector<multitype>X){
+std::string DecisionTree::predict(std::vector<std::string>X){
     if (this->tree == NULL){
-        std::cout<< "Model not trained"<< std::endl;
-        return -1;
+        std::cout<< "Model not trained"<< endLine;
+        return "-1";
     }
     else {
-        int index = 0;
         // Goes through the tree according to the index
-        Node root = *this->tree;
+        Node* r = this->tree;
+        std::string X_value = X[r->get_index()];
         while (true){
-            multitype X_value = X[root.get_index()];
-            for (auto feature_values: root.get_adjacents()){
-                if (feature_values.get_value() == X_value){
-                    root = feature_values;
+            for (auto feature_values: r->get_adjacents()){
+                // Atualizar o indice conforme o adjacente
+                if (feature_values->get_value() == X_value){
+                    // It is an attribute, it has just one adjacent
+                    r = feature_values->get_adjacents()[0];
                     break;
                 }
             }
             // Check if it is a leave
-            if (root.get_index() == this->n_features){
-                return root.get_value();
+            if (r->get_index() == this->n_features){
+                return r->get_value();
             }
+            // Update X
+            X_value = X[r->get_index()];
         }
     }
 }
 
-std::vector<multitype> DecisionTree::predict(std::vector<std::vector<multitype>>X){
-    std::vector<multitype> predictions;
+std::vector<std::string> DecisionTree::predict(std::vector<std::vector<std::string>>X){
+    std::vector<std::string> predictions;
     if (this->tree == NULL){
-        std::cout<< "Model not trained"<< std::endl;
+        std::cout<< "Model not trained"<< endLine;
         return {};
     }
     for (auto data: X){
-        multitype output = this->predict(data);
+        std::string output = this->predict(data);
         predictions.push_back(output);
     }
     return predictions;
--- DecisionTree.hpp
+++ DecisionTree.hpp
@@ -1,31 +1,38 @@
 #include "Node.hpp"
 #include "error.hpp"
+#include "utils.hpp"
 #include <iostream>
 #include <cmath>
 #include <map>
 #include <stack>
 #include <utility>
 
+#define endLine '\n'
+
 class DecisionTree{
     private:
         int n_features, n_classes;
         std::string criterion, target_name;
-        Node* tree;
+        Node* tree = NULL;
         std::vector<std::string> feature_names;
         // Store the feature nodes to merge the edge and a feature node
-        std::stack<Node> features;
+        std::stack<Node*> features;
 
-        std::map<multitype, std::map<multitype, int>> get_values(std::vector<multitype> sample, std::vector<multitype> classes);
-        // Get the classes for each attribute
-        double entropy(std::map<multitype, std::map<multitype, int>>values, int samples);
-        double gini(std::map<multitype, std::map<multitype, int>>values, int samples);
-        double gain(double entropy1, double entropy2);
+        std::map<std::string, std::map<std::string, int>> class_association(std::vector<std::string> sample, std::vector<std::string> classes);
+        // Get the distribution of the target data
+        std::map<std::string, int> c_distribution(std::vector<std::string> classes);
+        double entropy_S(std::map<std::string, int>values, int samples);
+        double entropy_a(std::map<std::string, std::map<std::string, int>>values, int samples);
+        double gini_S(std::map<std::string, int>values, int samples);
+        double gini_a(std::map<std::string, std::map<std::string, int>>values, int samples);
+        double gain(double set, double attribute);
     public:
         DecisionTree(std::string criterion);
+        ~DecisionTree();
         void names(std::vector<std::string>feature_names, std::string target_name);
         std::vector<std::string> get_features();
-        void fit(std::vector<std::vector<multitype>>X, std::vector<multitype>y);
-        multitype predict(std::vector<multitype>X);
-        std::vector<multitype> predict(std::vector<std::vector<multitype>>X);
+        void fit(std::vector<std::vector<std::string>>X, std::vector<std::string>y);
+        std::string predict(std::vector<std::string>X);
+        std::vector<std::string> predict(std::vector<std::vector<std::string>>X);
         Node* get_tree();
 };
